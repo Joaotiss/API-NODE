0e9e794cf5cfe4dd6b8d20a4232db926e98311c4
João <joaotiso09@gmail.com>
2025-07-22 13:23:59 -0300

    correção prisma

--- routes/public.js
+++ routes/public.js
@@ -1,16 +1,35 @@
-import express from 'express'
+import express from "express";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
 
-const router = express.Router()
+const router = express.Router();
+const prisma = new PrismaClient();
 
 // cadastro
 
-router.post("/cadastro", (req, res) => {
-  // lógica de cadastro
-  const user = req.body
+router.post("/cadastro", async (req, res) => {
+  try {
+    // lógica de cadastro
+    const user = req.body;
 
-  res.status(201).json(user)
-})
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(user.password, salt)
 
+    const userDB = await prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        password: hashPassword,
+      },
+    });
+    res.status(201).json(userDB);
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao cadastrar usuário" });
+  }
+});
 
+export default router;
 
-export default router
+//joaotiso09
+//h2BImA6C4iCT7SbD
+//mongodb+srv://joaotiso09:<db_password>@cluster0.lpasjhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
